49a7d86ac952368c00991960524d8b3ddf63d372
John Bunyan <john@thebunyans.plus.com>
2023-11-16 22:20:07 +0000

    setting up index.html as default for the static server

--- server.js
+++ server.js
@@ -3,10 +3,9 @@
 const express = require('express');
 
 const PORT = process.env.PORT || 4202;
-const INDEX = '/index.html';
 
 const server = express()
-  .use(express.static('dist'))
+  .use(express.static('dist', {index: "index.html"}))
   .listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
 function handle(signal) {
